89ef9cecd79fb3aa7583fc43e63bbcec8dc6accf
bb35319058 <63226641+bb35319058@users.noreply.github.com>
2020-04-20 15:36:31 +0900

    4/20 #1回目

--- k01/k01.c
+++ k01/k01.c
@@ -3,15 +3,18 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave);
+extern double var_online(double val,double ave,double square_ave);
+double N;
 
 int main(void)
 {
     double val;
+    double ave = 0,var = 0,square_ave = 0;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+    N = 0;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -27,6 +30,10 @@ int main(void)
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
 
+         N = N+1;
+         var = var_online(val, ave, square_ave);
+         ave = ave_online(val, ave);
+         square_ave = ave_online(pow(val, 2), square_ave);
 
     
 
@@ -39,9 +46,28 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    printf("ave_%1f\n", ave);
+    printf("var=%1f\n", var)
 
     return 0;
 
 
 }
 
+double ave_online(double val, double ave)
+{
+    double average;
+
+    average = ((N-1)/N * ave) + (1/N * val);
+
+    return average;
+}
+
+double var_online(double val, double ave, double square_ave)
+{
+    double variable;
+    
+    variable = {((N-1)/N * square_ave) + (1/N * pow(val,2))} - pow({((N-1)/N * ave) + (1/N * val)},2) 
+
+    return variable;
+}
