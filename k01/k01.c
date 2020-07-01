43e5d0179ed321a06f0d50989fa1f5b877b18505
bb35319058 <63226641+bb35319058@users.noreply.github.com>
2020-07-01 15:53:02 +0900

    update k01.c

--- k01/k01.c
+++ k01/k01.c
@@ -3,18 +3,18 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave);
-extern double var_online(double val,double ave,double square_ave);
+extern double ave_online(double val,double ave,int N);
+extern double var_online(double val,double ave,double var,double square_ave,int N);
 double N;
 
 int main(void)
 {
+    double ave = 0,var = 0,square_ave = 0,estimation_var=0;
+    int N=0;
     double val;
-    double ave = 0,var = 0,square_ave = 0;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
-    N = 0;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -30,44 +30,37 @@ int main(void)
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
 
-         N = N+1;
-         var = var_online(val, ave, square_ave);
-         ave = ave_online(val, ave);
-         square_ave = ave_online(pow(val, 2), square_ave);
-
-    
-
-
-
+        N++;
+         var = var_online(val, ave, var, square_ave, N);
+         ave = ave_online(val, ave, N);
+         square_ave = ave_online(pow(val, 2), square_ave, N);
     }
+
+    if(N==1){
+        estimation_var=0;
+    }
+    else{
+        estimation_var=N/(N-1)*var;
+    }
+
+    printf("ave=%1f\n", ave);
+    printf("var=%1f\n", var);
+    printf("estimation ave=%1f\n", ave);
+    printf("estimation var=%1f\n", estimation_var);
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
-    printf("ave_%1f\n", ave);
-    printf("var=%1f\n", var)
-
     return 0;
-
-
 }
 
-double ave_online(double val, double ave)
-{
-    double average;
-
-    average = ((N-1)/N * ave) + (1/N * val);
-
-    return average;
+double ave_online(double val, double ave, int N){
+    ave = (ave*(N-1)/N) + val/N;
+    return(ave);
 }
 
-double var_online(double val, double ave, double square_ave)
-{
-    double variable;
-    
-    variable = {((N-1)/N * square_ave) + (1/N * pow(val,2))} - pow({((N-1)/N * ave) + (1/N * val)},2) 
-
-    return variable;
+double var_online(double val, double ave, double var, double square_ave, int N){
+    var = (((N-1)*square_ave)/N + pow(val,2)/N -pow(((N-1)*ave)/N +val/N,2));
+    return(var);
 }
